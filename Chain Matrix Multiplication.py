618f7d629acb65bacc47941aceea5d6c70f232e2
Mansi Patel <69769132+mansi3012@users.noreply.github.com>
2021-10-03 15:50:19 +0530

    Update Chain Matrix Multiplication.py

--- Chain Matrix Multiplication.py
+++ Chain Matrix Multiplication.py
@@ -2,15 +2,21 @@
 
 def MatChainMul(a, n):
     dp = [[0 for i in range(n)] for j in range(n)]
+    
     for i in range(1, n):
         dp[i][i] = 0
 
     for L in range(2, n):
+        
         for i in range(1, n - L + 1):
+            
             j = i + L - 1
             dp[i][j] = 10 ** 10
+            
             for k in range(i, j):
+                
                 q = dp[i][k] + dp[k + 1][j] + a[i - 1] * a[k] * a[j]
+                
                 if q < dp[i][j]:
                     dp[i][j] = q
 
